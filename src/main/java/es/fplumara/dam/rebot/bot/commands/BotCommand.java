7f692f89edf11b729d63d45ab177035fb57067d1
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2026-02-10 20:35:16 +0100

    BotCommand interface

--- src/main/java/es/fplumara/dam/rebot/bot/commands/BotCommand.java
+++ src/main/java/es/fplumara/dam/rebot/bot/commands/BotCommand.java
@@ -5,8 +5,17 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BotCommand {
+public interface BotCommand {
+   //- **commands/BotCommand.java**
+    //
+    //    **Responsabilidad:** contrato para comandos.
+    //
+    //    Un comando debe poder:
+    //
+    //    - indicar el nombre del comando (ej: "!last")
+    //    - ejecutarse con acceso a:
+    //        - AppConfig
+    //        - FileService
+    //        - el evento de JDA
 
-    public BotCommand() {
-    }
 }
