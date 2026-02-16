16e4f770261356571680969d97317dba198736da
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2026-02-16 18:09:30 +0100

    Exceptions puedes indicar los detalles que saltaron (e)

--- src/main/java/es/fplumara/dam/rebot/exceptions/CommandException.java
+++ src/main/java/es/fplumara/dam/rebot/exceptions/CommandException.java
@@ -1,7 +1,13 @@
 package es.fplumara.dam.rebot.exceptions;
 
 public class CommandException extends RuntimeException {
+    private Exception parentException;
     public CommandException(String message) {
         super(message);
     }
+    public CommandException(String message,Exception e ){
+        super(message);
+        parentException = e;
+    }
 }
+
